1e6f7f3c37e15f437e927ed90e2e662aa853acae
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-11-04 21:39:50 +0530

    feat: pie chart for categories

---
+++ src/components/PieVariant.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import {
+  Cell,
+  Legend,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  Tooltip,
+} from "recharts";
+import CustomToolTip from "./CustomToolTip";
+import { format } from "date-fns";
+import { formatPercentage } from "@/lib/amountUtil";
+import CategoryToolTip from "./CategoryToolTip";
+
+type BarVariantProps = {
+  data: {
+    name: string;
+    value: number;
+  }[];
+};
+const COLORS = ["#0062ff", "#12c6ff", "#ff647f", "#ff9354"];
+const PieVariant = ({ data }: BarVariantProps) => {
+  return (
+    <ResponsiveContainer width={"100%"} height={350}>
+      <PieChart>
+        <Legend
+          layout="horizontal"
+          verticalAlign="bottom"
+          align="right"
+          iconType="circle"
+          content={({ payload }: any) => {
+            return (
+              <ul className="flex flex-col space-y-2">
+                {payload.map((entry: any, i: number) => (
+                  <li key={`item-${i}`} className="flex items-center space-x-2">
+                    <span
+                      className="size-2 rounded-full"
+                      style={{ backgroundColor: entry.color }}
+                    ></span>
+                    <div className="space-x-1">
+                      <span className="text-muted-foreground text-xs">
+                        {entry.value}
+                      </span>
+                      <span className="text-xs">
+                        {formatPercentage(entry.payload.percent * 100)}
+                      </span>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            );
+          }}
+        />
+        <Tooltip content={<CategoryToolTip></CategoryToolTip>}></Tooltip>
+        <Pie
+          data={data}
+          dataKey={"value"}
+          labelLine={false}
+          cx={"50%"}
+          cy={"50%"}
+          outerRadius={90}
+          innerRadius={60}
+          paddingAngle={2}
+          fill="#8884d8"
+        >
+          {data.map((_entry, i) => (
+            <Cell key={`cell-${i}`} fill={COLORS[i % COLORS.length]}></Cell>
+          ))}
+        </Pie>
+      </PieChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default PieVariant;
---
+++ src/components/SpendingPie.tsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { FileSearch2Icon, PieChartIcon, Radar, TargetIcon } from "lucide-react";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import PieVariant from "./PieVariant";
+
+type SpendingPieProps = {
+  data?: {
+    name: string;
+    value: number;
+  }[];
+};
+const SpendingPie = ({ data = [] }: SpendingPieProps) => {
+  const [chartType, setChartType] = useState("pie");
+
+  const onChartTypeChange = (type: string) => {
+    setChartType(type);
+  };
+  return (
+    <Card className="drop-shadow-sm">
+      <CardHeader className="flex space-y-2 lg:space-y-0 lg:flex-row lg:items-center justify-between">
+        <CardTitle className="text-xl line-clamp-1">Categories</CardTitle>
+        <Select defaultValue={chartType} onValueChange={onChartTypeChange}>
+          <SelectTrigger className="lg:w-auto h-9 rounded-md px-3">
+            <SelectValue placeholder="Chart Type" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="pie">
+              <div className="flex items-center">
+                <PieChartIcon className="size-4 mr-2 shrink-0" />
+                <p className="line-clamp-1">Pie Chart</p>
+              </div>
+            </SelectItem>
+            <SelectItem value="radar">
+              <div className="flex items-center">
+                <Radar className="size-4 mr-2 shrink-0" />
+                <p className="line-clamp-1">Radar Chart</p>
+              </div>
+            </SelectItem>
+            <SelectItem value="radial">
+              <div className="flex items-center">
+                <TargetIcon className="size-4 mr-2 shrink-0" />
+                <p className="line-clamp-1">Radial Chart</p>
+              </div>
+            </SelectItem>
+          </SelectContent>
+        </Select>
+      </CardHeader>
+      <CardContent>
+        {data.length === 0 ? (
+          <div className="flex flex-col gap-y-4 items-center justify-center h-[350px] w-full">
+            <FileSearch2Icon className="size-6 text-muted-foreground" />
+            <p className="text-muted-foreground text-sm">
+              No data for this period
+            </p>
+          </div>
+        ) : (
+          <>
+            {chartType === "pie" && <PieVariant data={data}></PieVariant>}
+            {/* {chartType === "radar" && <BarVariant data={data}></BarVariant>}
+            {chartType === "radial" && <LineVariant data={data}></LineVariant>} */}
+          </>
+        )}
+      </CardContent>
+    </Card>
+  );
+};
+
+export default SpendingPie;
